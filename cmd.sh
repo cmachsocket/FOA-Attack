92113457743fdfe170bc435761f307535a674068
cmachsocket <cmach_socket@outlook.com>
2026-05-25 16:52:51 +0800

    sync destination

--- cmd.sh
+++ cmd.sh
@@ -3,5 +3,6 @@ uv run generate_adversarial_samples_saliency.py model.saliency_loss_version=v1
 uv run generate_adversarial_samples_foa_attack.py --config-name ensemble_3models_gram            
 uv run generate_adversarial_samples_foa_attack.py --config-name ensemble_3models 
 uv run python llava_evaluate.py -m blackbox.model_name=llava
+uv run blackbox_text_generation.py blackbox.model_name=Qwen3.5-9B       
 CUDA_VISIBLE_DEVICES
 CUDA_VISIBLE_DEVICES=3 uv run vllm serve /home/gpuadmin/Qwen3.5-9B --served-model-name Qwen3.5-9B
